b8e767c8e1a94255280872c1b1553658cbec565f
danocorno <dbodonnell@gmail.com>
2016-09-13 08:27:54 -0500

    Add files via upload

---
+++ singing-telegrams-los-angeles.php
@@ -0,0 +1,317 @@
+
+<!doctype html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+<script type="text/javascript">
+		//<![CDATA[	
+		var winW = (window.innerWidth) ? window.innerWidth : document.documentElement.offsetWidth;
+		
+		if (winW > 800)
+			{
+			document.write('<!--[if lt IE 7 ]> <html lang="en" appcache="default.appcache" class="no-js width640 ie6"> <![endif]--> <!--[if IE 7 ]> <html lang="en" appcache="default.appcache" class="no-js widthwide ie7"> <![endif]--> <!--[if IE 8 ]> <html lang="en" appcache="default.appcache" class="no-js widthwide ie8"> <![endif]--> <!--[if IE 9 ]> <html lang="en" appcache="default.appcache" class="no-js widthwide ie9"> <![endif]--> <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" appcache="default.appcache" class="no-js widthwide"> <![endif]-->');
+			}
+		else
+		{
+		if (winW > 320)
+			{
+            document.write('<!--[if lt IE 7 ]> <html lang="en" appcache="default.appcache" class="no-js width640 ie6"> <![endif]--> <!--[if IE 7 ]>    <html lang="en" appcache="default.appcache" class="no-js width640 ie7"> <![endif]--> <!--[if IE 8 ]>    <html lang="en" appcache="default.appcache" class="no-js width640 ie8"> <![endif]--> <!--[if IE 9 ]>    <html lang="en" appcache="default.appcache" class="no-js width640 ie9"> <![endif]--> <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" appcache="default.appcache"  class="no-js width640"> <![endif]--> ' );
+			}
+		else
+			{
+			document.write('<!--[if lt IE 7 ]> <html lang="en" appcache="default.appcache" class="no-js width320 ie6"> <![endif]--> <!--[if IE 7 ]>    <html lang="en" appcache="default.appcache" class="no-js width320 ie7"> <![endif]--> <!--[if IE 8 ]>    <html lang="en" appcache="default.appcache" class="no-js width320 ie8"> <![endif]--> <!--[if IE 9 ]>    <html lang="en" appcache="default.appcache" class="no-js width320 ie9"> <![endif]--> <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" appcache="default.appcache"  class="no-js width320"> <![endif]--> ' );
+			}			
+		}		
+		//]]>
+</script>
+<noscript>
+	<!--[if (gt IE 9)|!(IE)]> <html lang="en" appcache="default.appcache"  class="no-js widthwide"> <![endif]-->
+</noscript>	
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta http-equiv="content-language" content="en" />
+	<meta name="description" content="FunnyGirl Telegrams:  Send a personal singing-telegram in the Los Angeles area for any occasion --from a highbrow event to an informal prank." />
+	<meta name="author" content="FunnyGirl Telegrams" />
+	<meta name="keywords" content="Singing Telegrams, entertainment, party, event, music, Marilyn Monroe, Los Angeles, LA" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>FunnyGirl Singing Telegrams | Marilyn and Friends - $99 | Los Angeles CA</title>
+	<!-- <link rel="canonical" href="//FunnyGirlTelegrams.com/" /> -->
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+	<link rel="stylesheet" href="css/style.php?v=20110802a" />
+	<script src="js/modernizr-1.7.min.js"></script>
+	<script src="js/flowplayer-3.2.6.min.js"></script>
+</head>
+<body>
+<div id="container1"><div id="container2"><div id="container3"><div id="container4">
+		
+		
+<header>
+	<img id="collage" class="imgWidthFull" src="images/collage.png" alt="FunnyGirl Singing Telegram Characters, Los Angeles CA: Marilyn Monroe, Lady Gaga, French Maid, Cheerleader, Valentine Gaga, Cupid, Naughty Nurse" title="FunnyGirl Singing Telegram Characters, Los Angeles CA: Marilyn Monroe, Lady Gaga, French Maid, Cheerleader, Valentine Gaga, Cupid, Naughty Nurse" />
+	<div class="brand">
+		<h1>FunnyGirl Telegrams<span>Singing Telegrams by Custom Characters in Los Angeles California</span></h1>
+		<div class="slogan">Marilyn and Friends - $99</div>
+	</div>
+</header>
+			
+		<div id="content">
+			<span class="phone"><strong><a  tabindex="1" href="tel:+13107495647">1.310.749.5647</a></strong></span>
+				<section>
+				<figure class="videoBox">
+				<h2 class="visuallyhidden">Marilyn and Friends Video</h2>
+				  <div class="video-js-box">
+				    <!-- Using the Video for Everybody Embed Code http://camendesign.com/code/video_for_everybody -->
+				    <video id="video" tabindex="2" class="video-js" width="512" height="288" controls poster="video/marilynFriends.jpg">
+				      <source src="video/marilynFriends.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+				      <source src="video/marilynFriends.webm" type='video/webm; codecs="vp8, vorbis"' />
+				      <source src="video/marilynFriends.ogv" type='video/ogg; codecs="theora, vorbis"' />
+				      <!-- Flash Fallback. Use any flash video player here. Make sure to keep the vjs-flash-fallback class. -->
+				      <object class="vjs-flash-fallback" width="512" height="288" type="application/x-shockwave-flash"
+				        data="http://builds.flowplayer.netdna-cdn.com/96111/55625/flowplayer-3.2.7-0.swf">
+				        <param name="movie" value="http://builds.flowplayer.netdna-cdn.com/96111/55625/flowplayer-3.2.7-0.swf" />
+				        <param name="allowfullscreen" value="true" />
+				        <param name="flashvars" value='config={"playlist":["video/marilynFriends.jpg", {"url": "video/marilynFriends.mp4","autoPlay":false,"autoBuffering":false}]}' />
+				        <!-- Image Fallback. Typically the same as the poster image. -->
+				        <img src="video/marilynFriends.jpg" width="512" height="288" alt="Marilyn and Friends video" title="No video playback capabilities." />
+				      </object>
+				    </video>
+				    <!-- Download links provided for devices that can't play video in the browser. -->
+				    <p class="vjs-no-video"><strong>Download Video:</strong>
+				      <a href="video/marilynFriends.mp4">MP4</a>,&nbsp;
+				      <a href="video/marilynFriends.webm">WebM</a>,&nbsp;
+				      <a href="video/marilynFriends.ogv">Ogg</a>
+				      <!-- HTML5 Video Player by VideoJS http://videojs.com -->
+				      
+				    </p>
+				  </div>
+					<figcaption id="videoDescription">
+						<a href="video/marilynFriends.mp4">Marilyn and Friends video</a> is a showcase of some possible characters for female singing telegrams in Los Angeles California. &nbsp;Download: &nbsp;<a href="video/marilynFriends.mp4">MP4</a> &nbsp;<a href="video/marilynFriends.webm">WebM</a> &nbsp;<a href="video/marilynFriends.ogv">Ogg</a>
+					</figcaption>
+				</figure>
+		</section>
+		<section>
+			<div class="col" id="col1">
+				<div class="sellingpointoneliner">	
+					
+			<div class="sellingpointonelinerimage" id="imageBlueRibbon">
+			&nbsp;
+			</div>
+					
+				Your top choice for outstanding in-person delivery  
+				</div>
+				<div class="sellingpoint">
+					<div class="imageBox">
+						<figure tabindex="3">
+							<img src="images/cop-female-singing-telegram-los-angeles.png" alt="Singing-Telegram Female Police Officer (COP) Los Angeles CA" title="Singing-Telegram Female Police Officer (COP) Los Angeles CA" width="90" height="226" />
+							<figcaption>Police Officer</figcaption>
+						</figure>
+					</div>	
+				<p>Would you like to send a singing telegram from a <strong>real singer</strong>?</p>
+				<p>Or, do you have a <strong>comedy prank</strong> that might run from G-rated and clean to R and slightly soiled?</p>
+				<p>Perhaps you just want to send someone a <strong>"get well"</strong> greeting, or a <strong>birthday</strong> visit from an unforgettable special guest.</p>
+				<p>Whatever you want, you will get exceptional service...and at a bargain price!</p>
+				</div>
+				<div class="sellingpointoneliner mobileNo">	
+					
+			<div class="sellingpointonelinerimage" id="imageHeart">
+			&nbsp;
+			</div>
+							<strong>FunnyGirl Telegrams</strong> will deliver your message in a way which is distinctive, original, and from the heart. 
+				</div>
+				<div class="sellingpoint mobileNo">
+				<h2>About FunnyGirl Telegrams:</h2>
+					<div class="imageBox">
+						<figure tabindex="4">
+							<img src="images/lady-gaga-valentine-heart-singing-telegram-los-angeles.png" alt="Singing-Telegram Lady Gaga Valentine Los Angeles CA" title="Singing-Telegram Lady Gaga Valentine Los Angeles CA" width="90" height="126" />
+							<figcaption>Valentine Lady Gaga</figcaption>
+						</figure>
+					</div>
+				<p>I am a <strong>singer, actor, and entertainer</strong> with over 10 years experience of  providing hundreds of singing telegrams to homes, offices, and public venues great and small!</p>
+				<p>I work  solo, or with a trusted group of performers who follow the same rule of <strong>giving you the best</strong>!</p>
+				<p>We love what we do, and we <strong>love making people happy</strong>!</p>
+				<p>Whether it's wowing someone "<strong>American Idol</strong>" style, getting goofy and spoofy, or delivering a sincere "apology gram", we all come <strong>from the heart</strong>.</p>  
+				</div>
+				<div class="sellingpoint mobileNo">
+					<div class="imageBox">
+						<figure tabindex="5">
+							<img src="images/cheerleader-singing-telegram-los-angeles.png" alt="Singing-Telegram Cheerleader Los Angeles CA" title="Singing-Telegram Cheeerleader Los Angeles CA" width="90" height="144" />
+							<figcaption>Cheerleader</figcaption>
+						</figure>
+					</div>	
+				<p><strong><a tabindex="6" href="http://en.wikipedia.org/wiki/Singing_telegram">Singing telegrams</a></strong> are a great way to <strong>celebrate</strong> and I think that everyone should try it at least once! &nbsp; I love seeing someone's smiling eyes when they realize that friends and family have taken the time to arrange the "<strong>surprise</strong>" - it's good times all around.</p>
+				</div>
+				<div class="sellingpointoneliner">
+					
+			<div class="sellingpointonelinerimage" id="imageTelephone">
+			&nbsp;
+			</div>
+							Call Now - <strong>Same Day Service</strong> Is Available! <br /><a tabindex="4" href="tel:+13107495647">1.310.749.5647</a>
+				</div>
+		
+			</div>
+			<div class="col" id="col2">
+				<div class="sellingpointoneliner mobileNo">	
+					
+			<div class="sellingpointonelinerimage" id="imageBalloon">
+			&nbsp;
+			</div>
+							Making Memories Is What I Do
+				</div>
+				<div class="sellingpoint mobileNo" id="memories">
+					<div class="imageBoxSet">
+					<div class="imageBox">
+						<figure tabindex="7">
+							<img src="images/gorilla-singing-telegram-los-angeles.png" alt="Singing-Telegram Gorilla Los Angeles CA" title="Singing-Telegram Gorilla Los Angeles CA" width="90" height="168" />
+							<figcaption>Gorilla</figcaption>
+						</figure>
+					</div>	
+					<div class="imageBox">
+						<figure tabindex="8">
+							<img src="images/marilyn-monroe-singing-telegram-los-angeles.png" alt="Marilyn Monroe Singing Telegram Los Angeles CA" title="Marilyn Monroe Singing Telegram Los Angeles CA" width="90" height="120" />
+							<figcaption>Marilyn Monroe</figcaption>
+						</figure>
+					</div>
+					<div class="imageBox">
+						<figure tabindex="9">
+							<img src="images/lady-gaga-singing-telegram-los-angeles.png" alt="Lady Gaga Singing Telegram Los Angeles CA" title="Lady Gaga Singing Telegram Los Angeles CA" width="90" height="179" />
+							<figcaption>Lady Gaga</figcaption>
+						</figure>
+					</div>
+					</div>
+				<h2>Here are some of  MY Standout Memories:</h2>
+					<ul>
+						<li><h3>Britney</h3> at the Catholic school</li>
+						<li>Dancing <h3>Gorilla</h3> at a Santa Monica wedding</li>
+						<li><h3>Marilyn M.</h3> at a centenarian's Bel-Air birthday (now that was a guy who really knew how to live!)</li>
+						<li><h3>Lady Gaga</h3> giving hair advice at a Pasadena Sweet Sixteen</li>
+						<li><h3>Classic tuxedo</h3> with an apology and a dozen roses which led to a couple's tearful re-union in Tustin (that was something - I don't think Dr. Phil could do better)</li>
+						<li>I had a lot of fun playing <h3>guitar</h3> and singing "On The Road Again" for someone in Beverly Hills about to – guess what – get on the road again!</li>
+						<li>And I  have to admit it was a real pleasure to be the <h3>Queen of England</h3> in Culver City.</li>
+					</ul>
+				</div>
+				<div class="sellingpoint mobileNo">
+				<h2 class="visuallyhidden">Music</h2>
+					<div class="imageBox">
+						<figure tabindex="10">
+							<img src="images/Tom-McEvilley-violin-los-angeles.jpg" alt="Tom McEvilley playing Violin in Los Angeles CA" title="Tom McEvilley playing Violin in Los Angeles CA" width="90" height="120" />
+							<figcaption>Musical Friends</figcaption>
+						</figure>
+					</div>
+					<p>And of course...<br /> <strong>Music, Music, Music</strong></p>
+					<p>My Musical Friends are <strong>opera singers</strong>, <strong>violinists</strong>, and <strong>guitarists</strong></p>
+					<p>So whatever you have in mind for your special someone... we can deliver...with superior style!</p>
+				</div>
+				<div class="sellingpoint">
+				<h2 class="visuallyhidden">Popular Characters</h2>
+					<div class="imageBoxSet">
+					<div class="imageBox">
+						<figure tabindex="11">
+							<img src="images/nurse-singing-telegram-los-angeles.png" alt="Singing-Telegram Nurse Los Angeles CA" title="Singing-Telegram Nurse Los Angeles CA" width="90" height="170" />
+							<figcaption>N'Aughtee Nurse</figcaption>
+						</figure>
+					</div>
+					<div class="imageBox">
+						<figure tabindex="12">
+							<img src="images/cupid-singing-telegram-los-angeles.png" alt="Singing-Telegram Cupid Los Angeles CA" title="Singing-Telegram Cupid Los Angeles CA" width="90" height="150" />
+							<figcaption>Cupid</figcaption>
+						</figure>
+					</div>	
+					<div class="imageBox">
+						<figure tabindex="13">
+							<img src="images/maid-singing-telegram-los-angeles.png" alt="French Maid Singing Telegram Los Angeles CA" title="French Maid Singing Telegram Los Angeles CA" width="90" height="154" />
+							<figcaption>French Maid</figcaption>
+						</figure>
+					</div>
+					</div>
+				<p>Here are some of my most <strong>Popular Characters</strong> (partial list –  it's always growing). &nbsp; Male Performers and Characters also available.</p>
+				<ul>
+				<li>Barbra</li>
+				<li>Bag lady</li>
+				<li>Biker chick</li>
+				<li>Cheerleader</li>
+				<li>Classic Tuxedo Singing Telegram</li>
+				<li>Cop</li>
+				<li>Cowgirl</li>
+				<li>Dorothy</li>
+				<li>French Maid</li>
+				<li>Gorilla</li>
+				<li>Hula Girl</li>
+				<li>Julie Androos</li>
+				<li>Lady Gaga</li>
+				<li>Liza</li>
+				<li>Mary Poppins</li>
+				<li>Marilyn Munroe</li>
+				<li>Mona the Mourner</li>
+				<li>Nerd</li>
+				<li>Nurse</li> 
+				<li>Pamela Andersun</li>
+				<li>Paris Hilltun</li>
+				<li>Princess</li>
+				<li>Queen of England</li>
+				<li>Shirley</li>
+				<li>Teena Turner</li>
+				<li>Yenta</li>
+				</ul>
+				AND MANY MORE! &nbsp;  JUST ASK FOR WHAT YOU WANT - SPECIAL REQUESTS WELCOME
+				</div>
+	
+			</div>		
+		</section>
+		<section id="contactMobile" class="mobileOnly">
+			<address>
+				<ul>
+					<li><h3>Phone:</h3> &nbsp;<a tabindex="14" href="tel:+13107495647">1.310.749.5647</a></li>
+					<li tabindex="15" ><h3>Email:</h3> &nbsp;<a href="mailto:Pamela@FunnyGirlTelegrams.com">Pamela@FunnyGirlTelegrams.com</a></li>
+				</ul>
+			</address>
+		</section>
+		<section id="contact" class="mobileNo">
+			<h2>Connection Options</h2>
+				
+				<address>
+					<ol>
+						<li><h3>Phone:</h3> &nbsp;<a tabindex="14" href="tel:+13107495647">1.310.749.5647</a></li>
+						<li tabindex="15" ><h3>Email:</h3> &nbsp;<a onmouseover="if(window.RegExp){this.href=unescape(this.href).replace(new RegExp('\\[put @ symbol here\\]'),'@');}" onfocus="this.onmouseover();" href="mailto:Pamela%5B%70%75%74%20%40%20%73%79%6D%62%6F%6C%20%68%65%72%65%5DFunnyGirlTelegrams.com">Send Pamela a note...</a></li>
+						
+						
+						 
+					</ol>
+				</address>
+				
+			</section>
+		<footer>
+	
+</footer>			
+		
+</div></div></div></div> <!--! end of #container -->
+
+<aside id="socialicons">
+	<p>'Like' or Review My Service:</p>
+	<div title="'Like' me on Google"><g:plusone></g:plusone></div>
+	<div title="'Check In' on Yelp"><a href="http://www.yelp.com/biz/funnygirl-telegrams-los-angeles"><img src="images/yelp.png" width="67" height="67" alt="Yelp" /></a></div>
+	<div title="Find me on Yahoo Local"><a href="http://local.yahoo.com/info-85545714-funnygirl-telegrams-los-angeles;_ylt=AmCmEdYtDuhZTdHK5fOteUiHNcIF;_ylv=3?csz=Los+Angeles%2C+CA"><img src="images/yahoo.png" width="67" height="67" alt="Yahoo" /></a></div>
+	<div title="Find me on Facebook"><a href="http://www.facebook.com/profile.php?id=100002578192161"><img src="images/facebook.png" width="67" height="67" alt="Facebook" /></a></div>
+	<div title="Find me on Google Places"><a href="http://maps.google.com/maps/place?cid=17809209457283989079&hl=en"><img src="images/google-places.png" width="36" height="67" alt="Google Places" /></a></div>
+	<div title="'Like' me on Facebook"><fb:like></fb:like></div>	
+</aside> 
+
+<div title="Find me on Party Pop"><a href="http://www.partypop.com/vendor.htm/4361299/"><img src="images/party-pop.png" width="67" height="39" alt="Party Pop" /></a></div>
+	
+</body>
+</html>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.5.1.js"%3E%3C/script%3E'))</script>
+<script src="js/plugins.js"></script>
+<script src="js/video.js"></script>
+<script src="https://apis.google.com/js/plusone.js"></script>
+<script src="js/script.js"></script>
+<script type="text/javascript">
+    // Add VideoJS to all video tags on the page when the DOM is ready
+    VideoJS.setupAllWhenReady();
+</script>
+<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>	
+		
+<!--[if lt IE 7 ]>
+	<script src="js/dd_belatedpng.js"></script>
+	<script> DD_belatedPNG.fix('img, .png_bg');</script>
+<![endif]-->
